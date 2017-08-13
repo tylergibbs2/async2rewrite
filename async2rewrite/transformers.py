6d93fcbb4b304b6a152baf05304f4e2d3f36bc37
Tyler <gibbstyler7@gmail.com>
2017-08-13 11:52:43 -0500

    Remove unnecessary classes

--- async2rewrite/transformers.py
+++ async2rewrite/transformers.py
@@ -10,7 +10,139 @@ easy_deletes_list = ['delete_custom_emoji', 'delete_channel', 'delete_invite', '
 easy_edits_list = ['edit_channel', 'edit_custom_emoji', 'edit_server']
 
 
-class CallTransforms:
+class DiscordTransformer(ast.NodeTransformer):
+
+    def visit_FormattedValue(self, node):
+        self.generic_visit(node)
+
+        return node
+
+    def visit_Module(self, node):
+        self.generic_visit(node)
+
+        return node
+
+    def visit_Expr(self, node):
+        self.generic_visit(node)
+
+        node = self.stateful_get_all_emojis(node)
+
+        return node
+
+    def visit_Call(self, node):
+        self.generic_visit(node)
+
+        # this all has to do with the stateful model changes
+        node = self.to_messageable(node)
+        node = self.easy_statefuls(node)
+        node = self.stateful_change_nickname(node)
+        node = self.stateful_create_channel(node)
+        node = self.easy_deletes(node)
+        node = self.stateful_edit_message(node)
+        node = self.easy_edits(node)
+        node = self.stateful_edit_role(node)
+        node = self.stateful_edit_channel_perms(node)
+        node = self.stateful_leave_server(node)
+        node = self.stateful_pin_message(node)
+        node = self.stateful_get_bans(node)
+        node = self.stateful_pins_from(node)
+
+        return node
+
+    def visit_arg(self, node):
+        self.generic_visit(node)
+
+        node.arg = node.arg.replace('server', 'guild').replace('Server', 'Guild')
+        return node
+
+    def visit_Attribute(self, node):
+        self.generic_visit(node)
+
+        node = self.to_edited_at(node)
+
+        node.attr = node.attr.replace('server', 'guild').replace('Server', 'Guild')
+        return node
+
+    def visit_Name(self, node):
+        self.generic_visit(node)
+
+        node.id = node.id.replace('server', 'guild').replace('Server', 'Guild')
+        return node
+
+    def visit_Await(self, node):
+        self.generic_visit(node)
+
+        return node
+
+    def visit_AsyncFunctionDef(self, node):
+        self.generic_visit(node)
+
+        node = self.ext_event_changes(node)
+        node = self.ensure_ctx_var(node)
+        node = self.remove_passcontext(node)
+
+        node.name = node.name.replace('server', 'guild').replace('Server', 'Guild')
+
+        return node
+
+    def visit_Assign(self, node):
+        self.generic_visit(node)
+
+        return node
+
+    @staticmethod
+    def ext_event_changes(coro):
+        if coro.name == 'on_command' or coro.name == 'on_command_completion':
+            coro.args.args = coro.args.args[1:]
+            return coro
+        elif coro.name == 'on_command_error':
+            coro.args.args.reverse()
+            return coro
+
+        return coro
+
+    @staticmethod
+    def ensure_ctx_var(coro):
+
+        coro_args = [arg.arg for arg in coro.args.args]
+
+        if not coro_args:
+            coro.args.args.append(ast.arg(arg='ctx', annotation=None))
+        elif 'self' in coro_args and 'ctx' not in coro_args:
+            coro.args.args.insert(1, ast.arg(arg='ctx', annotation=None))
+        elif 'self' not in coro_args and 'ctx' not in coro_args:
+            coro.args.args.insert(0, ast.arg(arg='ctx', annotation=None))
+
+        return coro
+
+    @staticmethod
+    def to_edited_at(attribute):
+        if attribute.attr == 'edited_timestamp':
+            attribute.attr = 'edited_at'
+
+        return attribute
+
+    @staticmethod
+    def stateful_get_all_emojis(expr):
+        if not isinstance(expr.value, ast.Await):
+            return expr
+        if not isinstance(expr.value.value, ast.Call):
+            return expr
+        call = expr.value.value
+        if isinstance(call.func, ast.Attribute):
+            if call.func.attr == 'get_all_emojis':
+                new_expr = ast.Expr()
+                new_expr.value = ast.Attribute()
+                new_expr.value.value = ast.Name()
+                new_expr.value.value.id = call.func.value.id
+                new_expr.value.value.ctx = ast.Load()
+                new_expr.value.attr = 'emojis'
+                new_expr.value.ctx = ast.Load()
+
+                new_expr = ast.copy_location(new_expr, expr)
+
+                return new_expr
+        return expr
 
     @staticmethod
     def remove_passcontext(n):
@@ -185,154 +317,3 @@ class CallTransforms:
                 call.func.attr = 'bans'
                 call.args = []
         return call
-
-
-class ExprTransforms:
-
-    @staticmethod
-    def stateful_get_all_emojis(expr):
-        if not isinstance(expr.value, ast.Await):
-            return expr
-        if not isinstance(expr.value.value, ast.Call):
-            return expr
-        call = expr.value.value
-        if isinstance(call.func, ast.Attribute):
-            if call.func.attr == 'get_all_emojis':
-                new_expr = ast.Expr()
-                new_expr.value = ast.Attribute()
-                new_expr.value.value = ast.Name()
-                new_expr.value.value.id = call.func.value.id
-                new_expr.value.value.ctx = ast.Load()
-                new_expr.value.attr = 'emojis'
-                new_expr.value.ctx = ast.Load()
-
-                new_expr = ast.copy_location(new_expr, expr)
-
-                return new_expr
-        return expr
-
-
-class AttributeTransforms:
-
-    @staticmethod
-    def to_edited_at(attribute):
-        if attribute.attr == 'edited_timestamp':
-            attribute.attr = 'edited_at'
-
-        return attribute
-
-
-class CoroTransforms:
-
-    @staticmethod
-    def ext_event_changes(coro):
-        if coro.name == 'on_command' or coro.name == 'on_command_completion':
-            coro.args.args = coro.args.args[1:]
-            return coro
-        elif coro.name == 'on_command_error':
-            coro.args.args.reverse()
-            return coro
-
-        return coro
-
-    @staticmethod
-    def ensure_ctx_var(coro):
-
-        coro_args = [arg.arg for arg in coro.args.args]
-
-        if not coro_args:
-            coro.args.args.append(ast.arg(arg='ctx', annotation=None))
-        elif 'self' in coro_args and 'ctx' not in coro_args:
-            coro.args.args.insert(1, ast.arg(arg='ctx', annotation=None))
-        elif 'self' not in coro_args and 'ctx' not in coro_args:
-            coro.args.args.insert(0, ast.arg(arg='ctx', annotation=None))
-
-        return coro
-
-
-class DiscordTransformer(ast.NodeTransformer):
-
-    def __init__(self):
-        self.calltransforms = CallTransforms()
-        self.exprtransforms = ExprTransforms()
-        self.attributetransforms = AttributeTransforms()
-        self.corotransforms = CoroTransforms()
-        super().__init__()
-
-    def visit_FormattedValue(self, node):
-        self.generic_visit(node)
-
-        return node
-
-    def visit_Module(self, node):
-        self.generic_visit(node)
-
-        return node
-
-    def visit_Expr(self, node):
-        self.generic_visit(node)
-
-        node = self.exprtransforms.stateful_get_all_emojis(node)
-
-        return node
-
-    def visit_Call(self, node):
-        self.generic_visit(node)
-
-        # this all has to do with the stateful model changes
-        node = self.calltransforms.to_messageable(node)
-        node = self.calltransforms.easy_statefuls(node)
-        node = self.calltransforms.stateful_change_nickname(node)
-        node = self.calltransforms.stateful_create_channel(node)
-        node = self.calltransforms.easy_deletes(node)
-        node = self.calltransforms.stateful_edit_message(node)
-        node = self.calltransforms.easy_edits(node)
-        node = self.calltransforms.stateful_edit_role(node)
-        node = self.calltransforms.stateful_edit_channel_perms(node)
-        node = self.calltransforms.stateful_leave_server(node)
-        node = self.calltransforms.stateful_pin_message(node)
-        node = self.calltransforms.stateful_get_bans(node)
-        node = self.calltransforms.stateful_pins_from(node)
-
-        return node
-
-    def visit_arg(self, node):
-        self.generic_visit(node)
-
-        node.arg = node.arg.replace('server', 'guild').replace('Server', 'Guild')
-        return node
-
-    def visit_Attribute(self, node):
-        self.generic_visit(node)
-
-        node = self.attributetransforms.to_edited_at(node)
-
-        node.attr = node.attr.replace('server', 'guild').replace('Server', 'Guild')
-        return node
-
-    def visit_Name(self, node):
-        self.generic_visit(node)
-
-        node.id = node.id.replace('server', 'guild').replace('Server', 'Guild')
-        return node
-
-    def visit_Await(self, node):
-        self.generic_visit(node)
-
-        return node
-
-    def visit_AsyncFunctionDef(self, node):
-        self.generic_visit(node)
-
-        node = self.corotransforms.ext_event_changes(node)
-        node = self.corotransforms.ensure_ctx_var(node)
-
-        node.name = node.name.replace('server', 'guild').replace('Server', 'Guild')
-        node = self.calltransforms.remove_passcontext(node)
-
-        return node
-
-    def visit_Assign(self, node):
-        self.generic_visit(node)
-
-        return node
