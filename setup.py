700a428fd4967dfc944bd55062cab1938eb031bb
ben simms <ben@bensimms.moe>
2017-09-24 21:44:18 +0100

    Fix: Just publish on pypi as a fork ...

--- setup.py
+++ setup.py
@@ -30,9 +30,6 @@ setup(
     download_url='https://github.com/TheTrain2000/async2rewrite/archive/{}.tar.gz'.format(version),
     keywords=['discord', 'discordpy', 'ast'],
     classifiers=[],
-    install_requires=['astunparse-noparen==1.5.0-noparen'],
-    dependency_links=[
-        "git+https://github.com/nitros12/astunparse.git#egg=astunparse-noparen-1.5.0-noparen"
-    ]
+    install_requires=['astunparse-noparen'],
 
 )
