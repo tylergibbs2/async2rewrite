26e45f66e446da47e2adc567b0934abb4b94348e
Tyler <gibbstyler7@gmail.com>
2017-10-16 21:11:25 -0500

    add support for multiple files

--- async2rewrite/main.py
+++ async2rewrite/main.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 import astunparse_noparen as astunparse
 
@@ -10,7 +11,8 @@ def get_result(code, **kwargs):
     include_ast = kwargs.pop('include_ast', False)
 
     def snowflake_repl(match):
-        return str(int(match.group(1)))
+        possible_snowflake = int(match.group(1))
+        return str(possible_snowflake)
 
     code = re.sub(r"""['\"](\d{17,19})['\"]""", snowflake_repl,
                   code)  # str snowflakes to int snowflakes
@@ -35,10 +37,22 @@ def get_result(code, **kwargs):
     return unparsed
 
 
-def from_file(file_path, **kwargs):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        code = f.read()
-        return get_result(code, **kwargs)
+def from_file(*files, **kwargs):
+    res = {}
+
+    def process_file(file):
+        with open(file, 'r', encoding='utf-8') as f:
+            res[file] = get_result(f.read(), **kwargs)
+
+    for path in files:
+        if path.endswith('.py'):  # it's a file
+            process_file(path)
+        else:  # it's a directory
+            for dirpath, _, filenames in os.walk(path, followlinks=True):
+                for file in filenames:
+                    process_file('{}/{}'.format(dirpath, file))
+
+    return res
 
 
 def from_text(text, **kwargs):
