7b366046843909013f4973b95c9343ed3b4159b1
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2018-09-25 19:23:14 +0100

    Revert If all else fails

--- application/launcher.py
+++ application/launcher.py
@@ -3,13 +3,7 @@ import os
 
 
 if __name__ == '__main__':
-    try:
-        if platform.system() == 'Linux':
-            os.system('nohup ./core.py &')
-        elif platform.system() == 'Windows':
-            os.system('start pythonw ./core.py')
-    except:
-        if platform.system() == 'Linux':
-            os.system('python3 ./core.py')
-        elif platform.system() == 'Windows':
-            os.system('python ./core.py')
+    if platform.system() == 'Linux':
+        os.system('python3 ./core.py')
+    elif platform.system() == 'Windows':
+        os.system('python ./core.py')
